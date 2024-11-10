e8b23199bad23d3a0dfe5da14dcd687dca5d3994
pankpanhh <pankpanhh@PANHPANHH>
2024-11-10 22:38:27 +0700

    Add project files.

---
+++ DoAnLTW/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+using DoAnLTW.Models;
+
+namespace DoAnLTW.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        QL_TX data = new QL_TX();
+        public ActionResult LoaiSP()
+        {
+            var l = data.Loais.ToList(); 
+            return View(l);
+        }
+
+    }
+}
